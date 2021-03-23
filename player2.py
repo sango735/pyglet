6030e909c0f5de6db0c42fc6165c4287a1cb47e1
sango735 <735th.floor@gmail.com>
2021-03-23 14:28:04 +0900

    矢印キーで移動出来るように設定

--- player2.py
+++ player2.py
@@ -2,3 +2,19 @@ from game import *
 
 image_player = image("chicken.png")
 
+def player(p):
+  #移動速度を設定
+  v = 0.01
+
+  if key(LEFT):
+    p.x -= v
+  
+  if key(RIGHT):
+    p.x += v
+
+  if key(UP):
+    p.y += v
+
+  if key(DOWN):
+    p.y -= v
+    
