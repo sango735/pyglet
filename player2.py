58a9cae690154bc4855a257d36a93af54eafec05
sango735 <735th.floor@gmail.com>
2021-03-24 16:57:35 +0900

    コメントアウト記述

--- player2.py
+++ player2.py
@@ -33,25 +33,36 @@ def player(p):
   p.x =max(-sw+p.sx,min(sw-p.sx,p.x))
   p.y =max(-sh+p.sy,min(sh-p.sy,p.y))
 
+  #出現する車の繰り返し処理を行う
+  #同様に１体１体に衝突判定をつける
   for e in group(enemy):
+    #距離を使った判定を行う為,math(数学).dist(距離、distance)関数を使う
+    #座標pと座標eの距離を求めて返し、右記の合計サイズよりも小さければ当たり判定を行う
     if math.dist((p.x, p.y),(e.x, e.y)) < (p.sx + e.sx)*0.5:
+      #衝突した時のエフェクトを発生させる
       new_crash(p.x,p.y,0.01,20,0.98)
-
       p.life=0
 
 def enemy(e):
   e.x += e.vx
+  #sin関数を使って振動の周波数と振り幅を設定する
   e.r= math.sin(e.time*0.2)*0.01
+  #時間を加算していく（車が動く）事で上記の振動を発生させる
   e.time +=1
+  #車が画面端に行った時に、その車を消去する条件式を設ける
   if abs(e.x) > sw+e.sx:
     e.life=0
 
 def stage(s):
+  #ランダム関数（0以上1未満の乱数を返す）を使って5%の確率で処理する条件式
   if random.random() < 0.05:
     size=0.1
+    #車が出現するX座標を設定したどちらかランダムの値で設定する
     side = random.choice([-1,1])
+    #車が出現するY座標の制限を決める。
+    #ランダムユニフォームは指定値の間の乱数を返す
     y=random.uniform(-sh+size,sh-size)
-
+    
     add(enemy,image_enemy,size,(sw+size)*side,y,-0.01*side)
 
 def start():
